bde67746b6a1ba0a65c50bc659521e33d5394bab
Tomoya Koyanagi <tomk79@gmail.com>
2025-07-09 18:27:16 +0900

    パブリッシュ前処理 を 却下
    プラグインが自動的にやってしまうのは問題があるかもしれない。
    プロジェクト側で簡単なカスタムプラグインを作れば同じことができ、プロジェクト側で自由に制御が可能なので、こちらの方法を検討する。

--- php/register.php
+++ php/register.php
@@ -19,21 +19,6 @@ class register {
 			return __CLASS__.'::'.__FUNCTION__.'('.( is_array($px) ? json_encode($px) : '' ).')';
 		}
 
-		// パブリッシュの前処理としてペライチコンテンツ生成を実行する
-		$pxcmd = $px->get_px_command();
-		if( count($pxcmd) >= 2 && $pxcmd[0] == 'publish' && $pxcmd[1] == 'run' ){
-			if(!$px->req()->get_param('path_region') && !$px->req()->get_param('paths_region') && !$px->req()->get_param('paths_ignore') && !$px->req()->get_param('keep_cache')){
-				// オプションなし(=フルパブリッシュ)のときにのみ実行する
-				set_time_limit(0);
-				$result = $px->internal_sub_request(
-					'/?PX=peraichize.create',
-					array()
-				);
-				echo $result;
-				set_time_limit(30);
-			}
-		}
-
 		// PX=peraichize を登録
 		$px->pxcmd()->register('peraichize', function($px) use ($conf){
 			$pxcmd = $px->get_px_command();
